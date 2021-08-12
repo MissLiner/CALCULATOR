d76a6ecbb8728b1f88b7c7506312d07a3b79213c
MissLiner <miss.liner@yahoo.com>
2021-08-12 19:26:13 -0400

    moved the elements around for aesthetics, fixed the numberBtns to get rid of 0 at the beginning of an input

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const display = document.getElementById("display");
 const calcBox = document.querySelector('#numBtnsBox');
+
 for (let i = 1; i <= 9; i++) {
     let numBtn = document.createElement('button');
     numBtn.textContent = `${i}`;
@@ -9,17 +10,18 @@ for (let i = 1; i <= 9; i++) {
     calcBox.appendChild(numBtn);
 }
 
-const numBtns = document.querySelectorAll(".numBtn");
-const operBtns = document.querySelectorAll(".operBtn");
-const eqlBtn = document.getElementById("eqlBtn");
-const decBtn = document.getElementById("decBtn");
-const delBtn = document.getElementById("delBtn");
-const negBtn = document.getElementById("negBtn");
+const numberBtns = document.querySelectorAll(".numBtn");
+const operatorBtns = document.querySelectorAll(".operBtn");
+const equalBtn = document.getElementById("eqlBtn");
+const clearBtn = document.getElementById("clrBtn");
+const decimalBtn = document.getElementById("decBtn");
+const deleteBtn = document.getElementById("delBtn");
+const negativeBtn = document.getElementById("negBtn");
 const background = document.getElementById("background");
 const notepad = document.getElementById("notepad");
 const clearPadBtn = document.getElementById("clearPadBtn");
-
 const allButtons = document.querySelectorAll("button");
+
 allButtons.forEach((button) => {
     button.addEventListener('click', () => {
         background.textContent += button.textContent;
@@ -28,7 +30,6 @@ allButtons.forEach((button) => {
         console.log(num2);
         console.log(result);
         console.log(operand);
-
     })
 })
 
@@ -39,25 +40,27 @@ let operand = "";
 
 updateDisplay = (content) => {
     display.textContent = content;
-    operBtns.forEach((button) => {
+    operatorBtns.forEach((button) => {
         button.classList.remove("highlight");
     })
-    //background.textContent += content;
 }
 
 function operate(operator, a, b) {
     if (operand === "/" && num2 === "0") {
         alert("Oops! Dividing by zero could break the universe . . .");
     }
-    else if (operator === "+") { return (a + b); }
-    else if (operator === "-") { return (a - b); }
-    else if (operator === "x") { return (a * b); }
-    else if (operator === "/") { return (a / b); }
+    else if (operator === "+") { return Math.round((a + b) * 100) / 100;}
+    else if (operator === "-") { return Math.round((a - b) * 100) / 100;}
+    else if (operator === "x") { return Math.round((a * b) * 100) / 100;}
+    else if (operator === "/") { return Math.round((a / b) * 100) / 100;}
+
+    Math.round((a + b) * 100) / 100;
 }
 
-numBtns.forEach((button) => {
+numberBtns.forEach((button) => {
     button.addEventListener('click', function newValue() {
             if (operand === "" && num1.length < 6) {
+                if (num1 === "0") {num1 = '';}
                 num1 += button.textContent;
                 updateDisplay(num1);
             }
@@ -68,19 +71,16 @@ numBtns.forEach((button) => {
                 updateDisplay(num1);
             }
             else if (operand !== "" && num2.length < 6) {
+                if (num2 === "0") {num2 = '';}
                 num2 += button.textContent;
                 updateDisplay(num2)
             }
-
-            //else {
-            //    num2
-            //}
     })
 })
 
-operBtns.forEach((button) => {
+operatorBtns.forEach((button) => {
     button.addEventListener('click', () => {
-        if (num2 == "") {
+        if (num1 !== "" && num2 === "") {
             num1 = parseFloat(num1);
             operand = button.textContent;
             button.classList.add("highlight");
@@ -88,33 +88,33 @@ operBtns.forEach((button) => {
         else if (num1 !== "" && num2 !== "" && operand !== "") {
             num2 = parseFloat(num2);
             result = operate(operand, num1, num2);
-            num1 = Math.round(result * 100) / 100;
             notepad.textContent += num1 + operand + num2 + "=" + result + "\r\n";
-            updateDisplay(num1);
+            num1 = result;
             num2 = "";
             result = "";
             operand = button.textContent;
+            background.textContent += num1;
+            updateDisplay(num1);
         }
-        //background.textContent += button.textContent;
     })
 })
 
-eqlBtn.addEventListener('click', () => {
+equalBtn.addEventListener('click', () => {
     if (num2 !== "") {
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
     result = operate(operand, num1, num2);
     notepad.textContent += num1 + operand + num2 + "=" + result + "\r";
-    num1 = Math.round(result * 100) / 100;
-    //background.textContent += num1;
+    num1 = result;
     num2 = "";
     result = "";
     operand = "start";
+    background.textContent += num1 + "  ";
     updateDisplay(num1);
-}
+    }
 })
 
-clrBtn.addEventListener('click', () => {
+clearBtn.addEventListener('click', () => {
     display.textContent = "0";
     num1 = "";
     num2 = "";
@@ -126,7 +126,7 @@ clrBtn.addEventListener('click', () => {
     })
 });
 
-delBtn.addEventListener('click', () => {
+deleteBtn.addEventListener('click', () => {
     if (operand !== "start") {
     if (num2 > 0) {
         num2 = num2.toString();
@@ -156,7 +156,7 @@ delBtn.addEventListener('click', () => {
 }
 })
 
-decBtn.addEventListener('click', () => {
+decimalBtn.addEventListener('click', () => {
     if (display.textContent.includes(".") === false || operand !== "") {
         if (operand === "start") {
             num1 = "0.";
@@ -178,7 +178,7 @@ decBtn.addEventListener('click', () => {
     }
 })
 
-negBtn.addEventListener('click', () => {
+negativeBtn.addEventListener('click', () => {
     if (num1 !== "" && num2 === "" && operand === "") {
         num1 = -num1;
         updateDisplay(num1);
